18b5b0282e1fee93da4ae8a80c7debf6bf968c95
Gianluca Grammatica <grammaticagianluca@gmail.com>
2025-09-13 09:04:38 +0200

    lalalalalalalalalà

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs');
+const ip = require('ip');
 
 const portNumber = 2000;
 const ipAddress = '127.0.0.1';
@@ -20,9 +21,41 @@ app.use(express.urlencoded({ extended: true }));
 // Template engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('home', { error: null });
+app.get(['/', '/index', 'home'], (req, res) => {
+  res.render('index', { error: null });
 });
+
+app.get(['/calc-subnet'], (req, res) => {
+  res.render('calc-subnet', { error: null });
+});
+
+app.post(['/calc-subnet'], (req, res) => {
+  let ip = req.body.ip;
+  let cidr = parseInt(req.body.cidr);
+
+    console.log(`Blup: ${ip} - ${cidr}`)
+
+  let array = ip.split(".");
+  let finalthing ="";
+
+  console.log(`eeeee: ${array}`)
+
+  array.forEach(cuartino => {
+    finalthing += toBase(cuartino, 2);
+  });
+
+  let neededBits = 32 - cidr;
+
+  console.log(finalthing)
+  let bitUsati = finalthing.slice(-neededBits)
+
+  console.log(`naaaa: ${finalthing} titi ${bitUsati}`)
+});
+
+
+function toBase(num, base){
+  return parseInt(num).toString(base);
+}
 
 app.listen(portNumber, ipAddress, () => {
   console.log(`Server ready at http://${ipAddress}:${portNumber}`)
